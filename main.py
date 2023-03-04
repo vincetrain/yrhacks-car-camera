ef738075ca6be301ddbeec4df4cfc83f8c2064bb
Vincent Tran <tran.vincent@outlook.com>
2023-03-04 16:22:41 -0500

    finalfinal

--- main.py
+++ main.py
@@ -1,338 +1,132 @@
-import cv2
+'''
+TODO:
+X Read data from camera / video 
+X Computer vision to detect cars 
+    - Calculate speed of each car and compare to local road's speed limit
+    - Read license plate of each car and log the license,time,location
+        - Only log if same data doesnt already exist
+            - Log time data to nearest half hour?
+    - Compare license to a database of stolen licenses
+        - Alert a user? / log to new database if stolen license matches.
+'''
+
+import time
 import numpy as np
-from skimage.filters import threshold_local
-import tensorflow as tf
-from skimage import measure
-import imutils
+import math
+import cv2
+import licenseocr
 
-def sort_cont(character_contours):
-    """
-    To sort contours from left to right
-    """
-    i = 0
-    boundingBoxes = [cv2.boundingRect(c) for c in character_contours]
-    (character_contours, boundingBoxes) = zip(*sorted(zip(character_contours, boundingBoxes),
-                                                      key=lambda b: b[1][i], reverse=False))
-    return character_contours
+CONFIDENCE_THRESHOLD = 0.45
+NMS_THRESHOLD = 0.2
 
+VEHICLES = ['car', 'truck', 'motorbike', 'bus']
 
-def segment_chars(plate_img, fixed_width):
-    """
-    extract Value channel from the HSV format of image and apply adaptive thresholding
-    to reveal the characters on the license plate
-    """
-    V = cv2.split(cv2.cvtColor(plate_img, cv2.COLOR_BGR2HSV))[2]
+## initializes model and network for cars
 
-    T = threshold_local(V, 29, offset=15, method='gaussian')
+class_file = open('./data/coco.names')
+class_names = class_file.read().splitlines()
 
-    thresh = (V > T).astype('uint8') * 255
+COLORS = np.random.uniform(0, 255, size=(len(class_names), 3))
 
-    thresh = cv2.bitwise_not(thresh)
+# worse accuracy, but less intensive. uncomment if cuda gpu is disabled in cv2
+model_config = 'cfg/yolov4-tiny.cfg'
+model_weights = 'yolov4-tiny.weights'
 
-    # resize the license plate region to a canoncial size
-    plate_img = imutils.resize(plate_img, width=fixed_width)
-    thresh = imutils.resize(thresh, width=fixed_width)
-    bgr_thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+# better accuracy, but more intensive. uncomment if cuda gpu is enabled in cv2
+# model_config = 'cfg/yolov4.cfg'
+# model_weights = 'yolov4.weights'
 
-    # perform a connected components analysis and initialize the mask to store the locations
-    # of the character candidates
-    labels = measure.label(thresh, neighbors=8, background=0)
+net = cv2.dnn.readNetFromDarknet(model_config, model_weights)
 
-    charCandidates = np.zeros(thresh.shape, dtype='uint8')
+net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
-    # loop over the unique components
-    characters = []
-    for label in np.unique(labels):
-        # if this is the background label, ignore it
-        if label == 0:
-            continue
-        # otherwise, construct the label mask to display only connected components for the
-        # current label, then find contours in the label mask
-        labelMask = np.zeros(thresh.shape, dtype='uint8')
-        labelMask[labels == label] = 255
+model = cv2.dnn_DetectionModel(net)
+model.setInputParams(size=(416, 416), scale=1/255, swapRB=True)
 
-        cnts = cv2.findContours(labelMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+## main loop
 
-        # ensure at least one contour was found in the mask
-        if len(cnts) > 0:
+def calculate_speed(old_pos, new_pos, fps):
+    # Calculate the distance between the two positions
+    distance = math.sqrt((new_pos[0] - old_pos[0])**2 + (new_pos[1] - old_pos[1])**2)
+    
+    # Calculate the speed by dividing the distance by the time between frames
+    speed = distance * fps
+    
+    return speed
 
-            # grab the largest contour which corresponds to the component in the mask, then
-            # grab the bounding box for the contour
-            c = max(cnts, key=cv2.contourArea)
-            (boxX, boxY, boxW, boxH) = cv2.boundingRect(c)
+car_tracker = {}
+car_speeds = {}
 
-            # compute the aspect ratio, solodity, and height ration for the component
-            aspectRatio = boxW / float(boxH)
-            solidity = cv2.contourArea(c) / float(boxW * boxH)
-            heightRatio = boxH / float(plate_img.shape[0])
+while(True):
+    cam = cv2.VideoCapture('examples/example-footage-1.mp4')
 
-            # determine if the aspect ratio, solidity, and height of the contour pass
-            # the rules tests
-            keepAspectRatio = aspectRatio < 1.0
-            keepSolidity = solidity > 0.15
-            keepHeight = heightRatio > 0.5 and heightRatio < 0.95
-
-            # check to see if the component passes all the tests
-            if keepAspectRatio and keepSolidity and keepHeight and boxW > 14:
-                # compute the convex hull of the contour and draw it on the character
-                # candidates mask
-                hull = cv2.convexHull(c)
-
-                cv2.drawContours(charCandidates, [hull], -1, 255, -1)
-
-    _, contours, hier = cv2.findContours(charCandidates, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    if contours:
-        contours = sort_cont(contours)
-        addPixel = 4  # value to be added to each dimension of the character
-        for c in contours:
-            (x, y, w, h) = cv2.boundingRect(c)
-            if y > addPixel:
-                y = y - addPixel
-            else:
-                y = 0
-            if x > addPixel:
-                x = x - addPixel
-            else:
-                x = 0
-            temp = bgr_thresh[y:y + h + (addPixel * 2), x:x + w + (addPixel * 2)]
-
-            characters.append(temp)
-        return characters
-    else:
-        return None
-
-
-
-class PlateFinder:
-    def __init__(self):
-        self.min_area = 4500  # minimum area of the plate
-        self.max_area = 30000  # maximum area of the plate
-
-        self.element_structure = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(22, 3))
-
-    def preprocess(self, input_img):
-        imgBlurred = cv2.GaussianBlur(input_img, (7, 7), 0)  # old window was (5,5)
-        gray = cv2.cvtColor(imgBlurred, cv2.COLOR_BGR2GRAY)  # convert to gray
-        sobelx = cv2.Sobel(gray, cv2.CV_8U, 1, 0, ksize=3)  # sobelX to get the vertical edges
-        ret2, threshold_img = cv2.threshold(sobelx, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-
-        element = self.element_structure
-        morph_n_thresholded_img = threshold_img.copy()
-        cv2.morphologyEx(src=threshold_img, op=cv2.MORPH_CLOSE, kernel=element, dst=morph_n_thresholded_img)
-        return morph_n_thresholded_img
-
-    def extract_contours(self, after_preprocess):
-        _, contours, _ = cv2.findContours(after_preprocess, mode=cv2.RETR_EXTERNAL,
-                                                    method=cv2.CHAIN_APPROX_NONE)
-        return contours
-
-    def clean_plate(self, plate):
-        gray = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
-        thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-        _, contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-        if contours:
-            areas = [cv2.contourArea(c) for c in contours]
-            max_index = np.argmax(areas)  # index of the largest contour in the area array
-
-            max_cnt = contours[max_index]
-            max_cntArea = areas[max_index]
-            x, y, w, h = cv2.boundingRect(max_cnt)
-            rect = cv2.minAreaRect(max_cnt)
-            rotatedPlate = plate
-            if not self.ratioCheck(max_cntArea, rotatedPlate.shape[1], rotatedPlate.shape[0]):
-                return plate, False, None
-            return rotatedPlate, True, [x, y, w, h]
-        else:
-            return plate, False, None
-
-
-
-    def check_plate(self, input_img, contour):
-        min_rect = cv2.minAreaRect(contour)
-        if self.validateRatio(min_rect):
-            x, y, w, h = cv2.boundingRect(contour)
-            after_validation_img = input_img[y:y + h, x:x + w]
-            after_clean_plate_img, plateFound, coordinates = self.clean_plate(after_validation_img)
-            if plateFound:
-                characters_on_plate = self.find_characters_on_plate(after_clean_plate_img)
-                if (characters_on_plate is not None and len(characters_on_plate) == 8):
-                    x1, y1, w1, h1 = coordinates
-                    coordinates = x1 + x, y1 + y
-                    after_check_plate_img = after_clean_plate_img
-                    return after_check_plate_img, characters_on_plate, coordinates
-        return None, None, None
-
-
-
-    def find_possible_plates(self, input_img):
-        """
-        Finding all possible contours that can be plates
-        """
-        plates = []
-        self.char_on_plate = []
-        self.corresponding_area = []
-
-        self.after_preprocess = self.preprocess(input_img)
-        possible_plate_contours = self.extract_contours(self.after_preprocess)
-
-        for cnts in possible_plate_contours:
-            plate, characters_on_plate, coordinates = self.check_plate(input_img, cnts)
-            if plate is not None:
-                plates.append(plate)
-                self.char_on_plate.append(characters_on_plate)
-                self.corresponding_area.append(coordinates)
-
-        if (len(plates) > 0):
-            return plates
-        else:
-            return None
-
-    def find_characters_on_plate(self, plate):
-
-        charactersFound = segment_chars(plate, 400)
-        if charactersFound:
-            return charactersFound
-
-    # PLATE FEATURES
-    def ratioCheck(self, area, width, height):
-        min = self.min_area
-        max = self.max_area
-
-        ratioMin = 3
-        ratioMax = 6
-
-        ratio = float(width) / float(height)
-        if ratio < 1:
-            ratio = 1 / ratio
-
-        if (area < min or area > max) or (ratio < ratioMin or ratio > ratioMax):
-            return False
-        return True
-
-    def preRatioCheck(self, area, width, height):
-        min = self.min_area
-        max = self.max_area
-
-        ratioMin = 2.5
-        ratioMax = 7
-
-        ratio = float(width) / float(height)
-        if ratio < 1:
-            ratio = 1 / ratio
-
-        if (area < min or area > max) or (ratio < ratioMin or ratio > ratioMax):
-            return False
-        return True
-
-    def validateRatio(self, rect):
-        (x, y), (width, height), rect_angle = rect
-
-        if (width > height):
-            angle = -rect_angle
-        else:
-            angle = 90 + rect_angle
-
-        if angle > 15:
-            return False
-        if (height == 0 or width == 0):
-            return False
-
-        area = width * height
-        if not self.preRatioCheck(area, width, height):
-            return False
-        else:
-            return True
-
-
-
-
-
-class NeuralNetwork:
-    def __init__(self):
-        self.model_file = "./model/binary_128_0.50_ver3.pb"
-        self.label_file = "./model/binary_128_0.50_labels_ver2.txt"
-        self.label = self.load_label(self.label_file)
-        self.graph = self.load_graph(self.model_file)
-        self.sess = tf.Session(graph=self.graph)
-
-    def load_graph(self, modelFile):
-        graph = tf.compat.v1.GraphDef()
-        graph_def = tf.compat.v1.GraphDef()
-        with open(modelFile, "rb") as f:
-            graph_def.ParseFromString(f.read())
-        with graph.as_default():
-            tf.import_graph_def(graph_def)
-        return graph
-
-    def load_label(self, labelFile):
-        label = []
-        proto_as_ascii_lines = tf.io.gfile.GFile(labelFile).readlines()
-        for l in proto_as_ascii_lines:
-            label.append(l.rstrip())
-        return label
-
-    def convert_tensor(self, image, imageSizeOuput):
-        """
-    takes an image and tranform it in tensor
-    """
-        image = cv2.resize(image, dsize=(imageSizeOuput, imageSizeOuput), interpolation=cv2.INTER_CUBIC)
-        np_image_data = np.asarray(image)
-        np_image_data = cv2.normalize(np_image_data.astype('float'), None, -0.5, .5, cv2.NORM_MINMAX)
-        np_final = np.expand_dims(np_image_data, axis=0)
-        return np_final
-
-    def label_image(self, tensor):
-
-        input_name = "import/input"
-        output_name = "import/final_result"
-
-        input_operation = self.graph.get_operation_by_name(input_name)
-        output_operation = self.graph.get_operation_by_name(output_name)
-
-        results = self.sess.run(output_operation.outputs[0],
-                                {input_operation.outputs[0]: tensor})
-        results = np.squeeze(results)
-        labels = self.label
-        top = results.argsort()[-1:][::-1]
-        return labels[top[0]]
-
-    def label_image_list(self, listImages, imageSizeOuput):
-        plate = ""
-        for img in listImages:
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-            plate = plate + self.label_image(self.convert_tensor(img, imageSizeOuput))
-        return plate, len(plate)
-
-
-if __name__ == "__main__":
-    findPlate = PlateFinder()
-
-    # Initialize the Neural Network
-    model = NeuralNetwork()
-
-    cap = cv2.VideoCapture('/example-footage.mp4')
-    while (cap.isOpened()):
-        ret, img = cap.read()
-        if ret == True:
-            cv2.imshow('original video', img)
-            if cv2.waitKey(25) & 0xFF == ord('q'):
-                break
-            # cv2.waitKey(0)
-            possible_plates = findPlate.find_possible_plates(img)
-            if possible_plates is not None:
-                for i, p in enumerate(possible_plates):
-                    chars_on_plate = findPlate.char_on_plate[i]
-                    recognized_plate, _ = model.label_image_list(chars_on_plate, imageSizeOuput=128)
-                    print(recognized_plate)
-                    cv2.imshow('plate', p)
-                    if cv2.waitKey(25) & 0xFF == ord('q'):
-                        break
-
-
-        else:
+    while(cam.isOpened()):
+        ret, frame = cam.read()
+        cam_width, cam_height, _ = frame.shape
+        if not ret:
             break
-    cap.release()
+        
+        start = time.time()
+        
+        classes, scores, boxes = model.detect(frame, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+
+        end = time.time()
+
+        for (class_id, score, box) in zip(classes, scores, boxes):
+            # print(class_names[class_id], score, box)
+            color = COLORS[int(class_id) % len(COLORS)]
+            class_name = class_names[class_id]
+            if (class_name in VEHICLES):
+                plate, conf = licenseocr.get_plate(frame[box[1]:box[1]+box[3], box[0]:box[0]+box[2]])
+                if plate == '' or conf < 20:
+                    plate = 'License Not Found'
+                label = "%s, %s : %f" % (class_name, plate, score)
+                cv2.rectangle(frame, box, color, 2)
+                cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+        
+                # check if a car is already being tracked
+                if class_id in car_tracker:
+                    # calculate speed
+                    new_x, new_y, new_w, new_h = box
+                    old_x, old_y, old_w, old_h, _ = car_tracker[class_id]
+                    speed = calculate_speed((old_x, old_y), (new_x, new_y), 2)
+                    car_speeds[class_id] = speed
+                    # update tracker
+                    car_tracker[class_id] = (new_x, new_y, new_w, new_h, time.time())
+                else:
+                    # add car to tracker
+                    car_tracker[class_id] = (box[0], box[1], box[2], box[3], time.time())
+                    car_speeds[class_id] = 0
+            
+        # display speed of each tracked car
+        for car_id, speed in car_speeds.items():
+            if speed is not None:
+                label = "Speed: {} km/h".format(round(speed, 2))
+                x, y, _, _ , _ = car_tracker[car_id]
+                cv2.putText(frame, label, (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+                    
+        
+        
+        
+        fps = 1 / (end-start) * 2
+        
+        fps_label = f'Model FPS: {fps:.2f}'
+        cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+        
+        cv2.namedWindow('Camera Footage', cv2.WINDOW_NORMAL)
+        cv2.imshow('Camera Footage', frame)
+        
+        key = cv2.waitKey(1)
+        
+        if key == ord('q'):
+            exit(0)
+        
+        if key == ord(' '):
+            while (cv2.waitKey(-1) != ord(' ')):
+                print('Video paused. Press [SPACE] to unpause.')
+        
+    cam.release()
+
     cv2.destroyAllWindows()
+
